66567f42abc8cc174569fa983b10117fa3654e34
zhoumiaode <1010467470@qq.com>
2018-08-30 15:50:23 +0800

    two

---
+++ DubboApi/src/main/java/com/dubbo/demo/api/Demo1Service.java
@@ -0,0 +1,17 @@
+package com.dubbo.demo.api;
+
+/**
+ * @ProjectName: SpringBootDubboDemo
+ * @Package: com.dubbo.demo.api
+ * @ClassName: Demo1Service
+ * @Description: java类作用描述
+ * @Author: zhoumiaode
+ * @CreateDate: 2018/08/30 14:52
+ * @UpdateUser: Neil.Zhou
+ * @UpdateDate: 2018/08/30 14:52
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ */
+public interface Demo1Service {
+    public String sayHello(String msg);
+}
