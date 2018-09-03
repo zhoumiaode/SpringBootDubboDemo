70955adee6041ea3bb6f2b8240ad8a4ed3943299
zhoumiaode <1010467470@qq.com>
2018-09-03 11:17:07 +0800

    a

---
+++ DubboApi/src/main/java/com/dubbo/demo/api/domain/TT.java
@@ -0,0 +1,16 @@
+package com.dubbo.demo.api.domain;
+
+/**
+ * @ProjectName: SpringBootDubboDemo
+ * @Package: com.dubbo.demo.api.domain
+ * @ClassName: TT
+ * @Description: java类作用描述
+ * @Author: zhoumiaode
+ * @CreateDate: 2018/09/03 11:16
+ * @UpdateUser: Neil.Zhou
+ * @UpdateDate: 2018/09/03 11:16
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ */
+public class TT {
+}
