9c828496f1ce130bc423f44976168e73f3fe563a
zhoumiaode <1010467470@qq.com>
2019-08-02 14:22:20 +0800

    the first mysql

--- DubboProvider/src/main/java/com/example/demo/provider/ProviderApplication.java
+++ DubboProvider/src/main/java/com/example/demo/provider/ProviderApplication.java
@@ -1,20 +1,28 @@
 package com.example.demo.provider;
 
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import com.dubbo.demo.api.DemoService;
+import com.dubbo.demo.api.domain.Girls;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 @EnableCaching
 @SpringBootApplication
 @EnableDubboConfiguration
 @MapperScan("com.example.demo.provider.mapper")
+@RestController
 public class ProviderApplication {
 
   /*  //DataSource配置
@@ -24,7 +32,15 @@ public class ProviderApplication {
        return new org.apache.tomcat.jdbc.pool.DataSource();
    }
 */
+    @Resource
+    private DemoService demoService;
+
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @GetMapping("test")
+    public List<Girls> getGirls(){
+        return demoService.getList();
     }
 }
