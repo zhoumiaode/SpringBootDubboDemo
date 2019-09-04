02d829083d96d692da0e6664b804f2493671541e
zhoumiaode <1010467470@qq.com>
2019-09-04 14:57:48 +0800

    sd

--- DubboProvider/src/main/java/com/example/demo/provider/Demo1ServiceImpl.java
+++ DubboProvider/src/main/java/com/example/demo/provider/Demo1ServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class Demo1ServiceImpl implements Demo1Service {
     @Override
     public String sayHello(String msg) {
-        return "Hello Wordssss";
+        return "Hello Word";
+
     }
 }
