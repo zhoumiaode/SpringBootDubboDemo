e94abd112e5e2343d7459742a1f6e2554585ef9a
zhoumiaode <1010467470@qq.com>
2019-09-12 09:24:26 +0800

    third

--- DubboProvider/src/main/java/com/example/demo/provider/DemoServiceImpl.java
+++ DubboProvider/src/main/java/com/example/demo/provider/DemoServiceImpl.java
@@ -31,7 +31,7 @@ public class DemoServiceImpl implements DemoService{
 
     @Override
     public String sayHello(String s) {
-        return "provierss";
+        return "proviserss";
     }
 
     @Override
